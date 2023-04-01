9c10a13be40d9f33b878d11ab299b77cef9fd584
Andrey Stolyarov <d10xa@mail.ru>
2023-04-01 15:55:49 +0300

    gulp-minify

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Build single html file
 
 ```
-npm i --save-dev gulp gulp-inline-source gulp-replace
+npm i --save-dev gulp gulp-inline-source gulp-replace gulp-minify
 sbt fullLinkJS
 ./node_modules/gulp-cli/bin/gulp.js
 open target/html/index.html
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp');
 const inlinesource = require('gulp-inline-source');
 const replace = require('gulp-replace');
+const minify = require('gulp-minify');
 
-gulp.task('default', () => {
+gulp.task('minify-js', function() {
+  return gulp.src('./target/scala-3.2.2/*-opt/*.js')
+   .pipe(minify())
+    .pipe(gulp.dest('./target/gulp'));
+});
+
+gulp.task('build-single-html', function() {
   return gulp
     .src('./index.html')
     .pipe(replace('.js"></script>', '.js" inline></script>'))
@@ -15,3 +22,5 @@ gulp.task('default', () => {
     )
     .pipe(gulp.dest('./target/html'));
 });
+
+gulp.task('default', gulp.series('minify-js', 'build-single-html'));
